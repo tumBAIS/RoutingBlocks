d1916c215b2d88be421b449808ef0e249bc03dbd
Patrick Sean Klein <patrick@libklein.com>
2023-04-08 16:19:49 +0200

    Replace std::get variant access with std::get_if

--- bindings/src/Solution.cpp
+++ bindings/src/Solution.cpp
@@ -366,15 +366,18 @@ namespace vrpis::bindings {
               [](Evaluation& evaluation, const Instance& instance, const Route& route,
                  int after_pos, std::variant<VertexID, const Vertex*, const Node*> node) -> cost_t {
                   auto after_iter = std::next(route.begin(), after_pos);
-                  if (std::holds_alternative<VertexID>(node)) {
+
+                  if (auto* v_id_ptr = std::get_if<VertexID>(&node)) {
                       return evaluate_insertion(evaluation, instance, route, after_iter,
-                                                instance.getVertex(std::get<VertexID>(node)));
-                  } else if (std::holds_alternative<const Vertex*>(node)) {
+                                                instance.getVertex(*v_id_ptr));
+                  } else if (auto* vertex_ptr = std::get_if<const Vertex*>(&node)) {
                       return evaluate_insertion(evaluation, instance, route, after_iter,
-                                                *std::get<const Vertex*>(node));
+                                                **vertex_ptr);
+                  } else if (auto* node_ptr = std::get_if<const Node*>(&node)) {
+                      return evaluate_insertion(evaluation, instance, route, after_iter,
+                                                (*node_ptr)->vertex());
                   } else {
-                      return evaluate_insertion(evaluation, instance, route, after_iter,
-                                                std::get<const Node*>(node)->vertex());
+                      throw std::runtime_error("Invalid node type!");
                   }
               });
 
