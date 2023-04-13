236cdccf78f49de39b320faad3f7ea9b0f4100d1
Patrick Sean Klein <patrick@libklein.com>
2023-04-13 17:15:26 +0200

    Add stub for ADPTW and NIFTW evaluation classes.

--- bindings/stubs/_adptw.pyi
+++ bindings/stubs/_adptw.pyi
@@ -14,3 +14,11 @@ def create_adptw_vertex(vertex_id: int, str_id: str, is_station: bool, is_depot:
 
 
 def create_adptw_arc(data: ADPTWArcData) -> Arc: ...
+
+
+class ADPTWEvaluation(Evaluation):
+    overload_penalty_factor: float
+    overcharge_penalty_factor: float
+    time_shift_penalty_factor: float
+
+    def __init__(self, vehicle_battery_capacity: resource_t, vehicle_storage_capacity: resource_t) -> None: ...
--- bindings/stubs/_niftw.pyi
+++ bindings/stubs/_niftw.pyi
@@ -15,3 +15,11 @@ def create_niftw_vertex(vertex_id: int, str_id: str, is_station: bool, is_depot:
 
 
 def create_niftw_arc(data: NIFTWArcData) -> Arc: ...
+
+
+class NIFTWEvaluation(Evaluation):
+    overload_penalty_factor: float
+    overcharge_penalty_factor: float
+    time_shift_penalty_factor: float
+
+    def __init__(self, vehicle_battery_capacity: resource_t, vehicle_storage_capacity: resource_t) -> None: ...
