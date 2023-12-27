2d2ee06c79b5d458b1aeeafd95a2b42b5441079a
Patrick Sean Klein <patrick@libklein.com>
2023-12-28 00:01:47 +0100

    Add license headers to sources

--- test/tests/benchmarks/test_benchmark_frvcp.py
+++ test/tests/benchmarks/test_benchmark_frvcp.py
@@ -1,23 +1,32 @@
+# Copyright (c) 2023 Patrick S. Klein (@libklein)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from __future__ import annotations
 
-import itertools
-import math
 import random
-import time
-from collections import defaultdict
-from pathlib import Path
-from typing import Tuple, Callable, Dict, List, Iterable
+from typing import List
 
 import pytest
 
-import helpers
-
 from fixtures import *
 
-import sys
-
 try:
-    import routingblocks as alns
     from routingblocks import adptw
 except ModuleNotFoundError:
     pass
@@ -35,7 +44,7 @@ def test_frvcp_benchmark_propagators(instance_parser,
                                      random_raw_route_factory, propagator, benchmark):
     py_instance, instance = instance_parser('r101_21.txt')
     factories = {
-        'adptw-cpp': lambda: adptw.FRVCP(instance, py_instance.parameters.battery_capacity_time)
+        'adptw-cpp': lambda: adptw.FacilityPlacementOptimizer(instance, py_instance.parameters.battery_capacity_time)
     }
 
     # Create routes
